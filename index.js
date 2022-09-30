c0d0804e33d7b2214644ec3ba760de63e02161c4
MariaLorrana913 <103418874+LorranaA1@users.noreply.github.com>
2022-09-30 17:01:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let nome = "Maria";
+alert("Meu nome é "+nome);
+document.write("Meu nome é..."+nome);
+document.getElementById("nome").innerHTML = "Meu primeiro texto <b>JS<b>";
+console.log("Meu nome é console");
+
+function minhaFuncao(){
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+}
+
+minhaFuncao(); 
